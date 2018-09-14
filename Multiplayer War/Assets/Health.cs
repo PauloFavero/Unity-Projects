cb95d2904e8a72fc2dfaf5ea49111e424e3cc2eb
Favero Pereira <phfaverop@gmail.com>
2018-09-14 17:18:03 +0200

    Game Multiplayer War
    Bug on the HUD Life

---
+++ Multiplayer War/Assets/Health.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour {
+
+    public const int maxHealth = 100;
+    public RectTransform healthBar;
+
+    public int currentHealth = maxHealth;
+
+    public void TakeDamage(int amount){
+
+        currentHealth -= amount;
+
+        if(currentHealth <= 0){
+            currentHealth = 0;
+            Debug.Log("Dead!");
+        }
+
+        healthBar.sizeDelta = new Vector2(currentHealth, healthBar.sizeDelta.y);
+    }
+}
